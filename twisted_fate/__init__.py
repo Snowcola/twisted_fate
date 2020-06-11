8ac0a7dc010cb25cacde82fe5fa2fc405e4dfeca
Anthony Keelan <anthony.keelan@gmail.com>
2020-06-11 13:04:32 -0600

    bump version for pypi

--- twisted_fate/__init__.py
+++ twisted_fate/__init__.py
@@ -1,5 +1,5 @@
 name = "twisted_fate"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 from .api_wrapper.deck import Deck
 from .api_wrapper.card import Card
 from .api_wrapper.active_game import Game, GameFrame, Screen
